15e1bf3e0096af87ae6cee2508ac4667afc6a113
Fatemeh Taher <Hyuuga_neji1375@yahoo.com>
2025-01-13 17:00:22 +0330

    add template

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
