9f49a85f5551393fbbd50d2a02a3cfe0c091a242
Fatemeh Taher <Hyuuga_neji1375@yahoo.com>
2025-01-15 15:28:18 +0330

    Change direction of the template

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 app = Flask(__name__)
+
+# Configure Babel
+app.config['BABEL_DEFAULT_LOCALE'] = 'fa'
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
+
+babel = Babel(app)
+
+# Function to get the current locale
+def get_locale():
+    return request.args.get('lang', app.config['BABEL_DEFAULT_LOCALE'])
+
+# Initialize Babel
+babel.init_app(app, locale_selector=get_locale)
 
 @app.route('/')
 def home():
