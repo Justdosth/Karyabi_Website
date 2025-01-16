80f38d8d20de1e0a3f2c82e563c5514681aee4e0
Fatemeh Taher <Hyuuga_neji1375@yahoo.com>
2025-01-16 20:26:57 +0330

    add rtl bootstrap, change content to Persian, add change language btn, add fonts for both languages

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_babel import Babel, _
 
 app = Flask(__name__)
 
 # Configure Babel
+app.config['LANGUAGES'] = ['en', 'fa']  # English and Persian
 app.config['BABEL_DEFAULT_LOCALE'] = 'fa'
 app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
 
@@ -16,13 +17,21 @@ def get_locale():
 # Initialize Babel
 babel.init_app(app, locale_selector=get_locale)
 
+# Function to determine text direction
+def get_direction(lang):
+    return 'rtl' if lang == 'fa' else 'ltr'
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    lang = get_locale()  # Get the current language
+    direction = get_direction(lang)  # Determine the text direction
+    return render_template('index.html', lang=lang, direction=direction)
 
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
 
 if __name__ == "__main__":
+    with app.test_request_context():
+        print(url_for('static', filename='en'))  # Should output "/?lang=en"
     app.run(debug=True)
